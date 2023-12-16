735047767851dfe1844c9f57b90d791733af36c7
João Neto <jflorencioadv@gmail.com>
2023-12-16 15:35:49 +0100

    fetch-async-await

--- js/script.js
+++ js/script.js
@@ -1,55 +1,102 @@
-const showPokemons = document.getElementById('app');
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+const perPage = 10;
+let currentPage = 1;
+
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
-const searchBtn = document.getElementById('searchBtn');
-const searchInput = document.getElementById('searchInput');
+const resetBtn = document.getElementById('resetBtn');
+const appContainer = document.getElementById('app');
 
-// aqui me traigo el catalogo de pokemons
+if (searchBtn) {
+    searchBtn.addEventListener('click', handleSearch);
+}
 
+if (prevBtn) {
+    prevBtn.addEventListener('click', () => goToPage(currentPage - 1));
+}
 
+if (nextBtn) {
+    nextBtn.addEventListener('click', () => goToPage(currentPage + 1));
+}
 
-const getPokemons = async () => {
+if (resetBtn) {
+    resetBtn.addEventListener('click', () => goToPage(1));
+}
+
+async function fetchData(url) {
     try {
-    
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
+        const response = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error ("La lista de Pokemon no se pudo cargar");
+        // Check if the response is successful
+        if (!response.ok) {
+            throw new Error('Pokemon not found');
         }
-        const pokemons = await res.json();
-        console.log(pokemons);
-    
-        // const pokemonName = pokemons.results.name;
-        
-        
-        printPokemons(pokemons);
-        
+
+        const data = await response.json();
+
+        if (Array.isArray(data.results)) {
+            displayData(data.results);
+        } else if (data.name) {
+            // If it's a single Pokemon, wrap it in an array for consistent handling
+            displayData([data]);
+        } else {
+            console.error('Unexpected data format:', data);
+        }
     } catch (error) {
-        console.log(error);
+        console.error('Error fetching data:', error);
+
+        // Display an error message in the appContainer
+        appContainer.innerHTML = `<p class="error-message">The Pokémon you are looking for does not exist in the Pokédex.</p>`;
     }
-    
+}
 
-};
+function displayData(pokemonList) {
+    appContainer.innerHTML = '';
 
-getPokemons();
+    if (pokemonList.length === 0) {
+        appContainer.innerHTML = '<p>No matching Pokémon found.</p>';
+    } else {
+        pokemonList.forEach(pokemon => {
+            const card = createPokemonCard(pokemon);
+            appContainer.innerHTML += createPokemonCard(pokemon)
+        });
+    }
+}
 
+function createPokemonCard(pokemon) {
+    const card = `<div class="pokemon-card">
+      <img src=https://img.pokemondb.net/sprites/home/normal/${pokemon.name}.png alt=${pokemon.name}/>
+      <p>${capitalizeFirstLetter(pokemon.name)}</p>
+    </div>`
 
-const printPokemons = (pokemons) => {
-    const pintarPokemons = pokemons.results;
-    pintarPokemons.forEach(elementPok => {
-        const pokemonName = elementPok.name;
-        // const pokeImagen = elementPok.sprites.back_default;
-         
-        showPokemons.innerHTML += `<li>
-        <p>Nombre: ${pokemonName}</P>
-        </li>`
-    });
-};
+    return card;
+}
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
+function goToPage(page) {
+    if (page >= 1) {
+        currentPage = page;
+        const url = `${apiUrl}?limit=${perPage}&offset=${(currentPage - 1) * perPage}`;
+        fetchData(url);
+    }
+}
 
-searchBtn.addEventListener('click', () => {
-    const findPokemon = searchInput.value;
-    (`https://pokeapi.co/api/v2/pokemon/${findPokemon}`)
+async function handleSearch() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm.length >= 3) {
+        const searchUrl = `${apiUrl}/${searchTerm}`;
+        await fetchData(searchUrl);
+    } else {
+        const url = `${apiUrl}?limit=${perPage}&offset=${(currentPage - 1) * perPage}`;
+        await fetchData(url);
+    }
+}
 
-});
+// Initial load
+const initialUrl = `${apiUrl}?limit=${perPage}&offset=${(currentPage - 1) * perPage}`;
+fetchData(initialUrl);
